4fd0b67ddf89849ef271b4338b7882c261163760
DELL-PC <DELL-PC@DESKTOP-QH3RS8F>
2020-05-15 00:19:18 +0530

    corona_project

---
+++ src/hackathon/Hackathon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackathon;
+
+/**
+ *
+ * @author DELL-PC
+ */
+public class Hackathon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        start s =new start();
+        s.setVisible(true);
+    }
+    
+}
